10be45dfe12fc15fd7c55a65730c5071188551c3
Gotas <mr.gotas@mail.ru>
2024-04-01 10:35:41 +0700

    Изменеине части кода с прямоугольником

--- rectangle.cpp
+++ rectangle.cpp
@@ -7,6 +7,10 @@ int main() {
 	int oneSd, twoSd;
 	cout << "Введите длины двух сторон прямоугольника:" << endl;
 	cin >> oneSd >> twoSd;
+	if (oneSd <= 0 || twoSd <= 0) {
+		cout << "Нужны положительные стороны" << endl;
+		return 0;
+	}
 	cout << "Периметр: " << (oneSd + twoSd) * 2 << endl;
 	cout << "Площадь: " << oneSd * twoSd << endl;
 	cout << "Длина каждой диагонали:" << pow(pow(oneSd, 2) + pow(twoSd, 2), 0.5) << endl;
